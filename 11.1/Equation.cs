9cd11a0f3faed5b8fffc19fc918f7f79f3d7d3ee
mishantique <michvitko@mail.ru>
2024-02-10 21:39:17 +0300

    Добавьте файлы проекта.

---
+++ 11.1/Equation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+    struct Equation
+    {
+        double k;
+        double b;
+        public Equation(double k, double b) 
+        { 
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root() 
+        {   
+            if (k != 0)
+            {
+                double root = -b / k;
+                return $"Решение уравнения: x = {root}";
+            }
+            else
+            {
+                return $"Решение уравнения: x - любое";
+            }
+            
+        }
+    }
+}
---
+++ 11.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа решает уравнение вида kx + b = 0\nВведите коэффициент k:");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэффициент b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k, b);
+            // Создаём экземпляр без ввода
+            Equation equation12 = new Equation(k = 1, b = 2);
+
+            Console.WriteLine("Для экземпляра equation12 {0}", equation12.Root());
+            Console.WriteLine("Для экземпляра с коэффициентами с клавиатуры {0}", equation.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
